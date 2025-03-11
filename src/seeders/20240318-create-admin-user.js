691ffd439da9e7e000e5c045e89359f92204e636
prokopenko.a <kap.moral22@gmail.com>
2025-03-12 01:37:25 +0300

    meinor

--- src/seeders/20240318-create-admin-user.js
+++ src/seeders/20240318-create-admin-user.js
@@ -1,1 +1,24 @@
- 
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const passwordHash = await bcrypt.hash('admin123', 10);
+    
+    await queryInterface.bulkInsert('users', [{
+      username: 'admin',
+      email: 'admin@mall.com',
+      password_hash: passwordHash,
+      role: 'admin',
+      first_name: 'System',
+      last_name: 'Administrator',
+      is_active: true,
+      created_at: new Date(),
+      updated_at: new Date()
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('users', { email: 'admin@mall.com' }, {});
+  }
+}; 
